51863699fb47605934daa2ff1237d5fd8d23406e
Kozman1 <markozyk@gmail.com>
2022-02-09 14:17:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = '...';
+	$: isMark = name.toUpperCase() === 'MARK';
+</script>
+
+<main>
+	<h1>Hello <span class:highlight={isMark}>{name}</span>!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input bind:value={name} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.highlight {
+		color: blue;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
